b850dc99e12f197a509a2ecc661c61c3ba196103
salah azab <salahazab98@hotmail.com>
2022-03-03 17:46:53 +0200

    third-upload

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import "./sass/style.scss";
 import "@laylazi/bootstrap-rtl-scss/scss/bootstrap-rtl.scss";
+import "./sass/style.scss";
 import "@fortawesome/fontawesome-free/js/all.min.js";
+import "@laylazi/bootstrap-rtl/dist/js/bootstrap.min.js";
 import 'jquery/dist/jquery.min';
+import "popper.js/dist/popper.min.js";
+
 
