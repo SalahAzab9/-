73966756c35830a59827cc59962b9f1eb08c5887
salah azab <salahazab98@hotmail.com>
2022-03-07 23:22:36 +0200

    final-upload

--- src/index.js
+++ src/index.js
@@ -11,3 +11,19 @@ $(function(){
     
     $("#save").text(date.getFullYear());
 })
+
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      var forms = document.getElementsByClassName('needs-validation');
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
